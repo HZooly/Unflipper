1850ff6bf260a00cf3700bdbb932edc15a6fe439
Hugo Torzuoli <torzuoli.hugo@gmail.com>
2019-07-18 11:49:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ const Discord = require('discord.js')
 require('dotenv').config()
 const client = new Discord.Client()
 
+const TABLEFLIP = '(╯°□°）╯︵ ┻━┻'
 const UNFLIPS = ['┬─┬ ノ( ゜-゜ノ)', '┳━┳ ヽ(ಠل͜ಠ)ﾉ', '┬─┬ ಠ_ಠᕗ']
 
 client.on('ready', () => {
@@ -11,7 +12,7 @@ client.on('ready', () => {
 
 client.on('message', (message) => {
   if (message.author.bot) return
-  if (message.content === '(╯°□°）╯︵ ┻━┻') message.channel.send(UNFLIPS[Math.floor(Math.random() * UNFLIPS.length)])
+  if (message.content.includes(TABLEFLIP)) message.channel.send(UNFLIPS[Math.floor(Math.random() * UNFLIPS.length)])
 })
 
 client.login(process.env.DISCORD_TOKEN)
